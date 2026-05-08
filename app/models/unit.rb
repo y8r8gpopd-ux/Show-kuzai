53064be46a23141943b2458ac6425dd52b6dbec3
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-05-08 22:33:04 +0900

    ActiveHashのunitモデル内のIdミス修正

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -5,11 +5,11 @@ class Unit < ActiveHash::Base
     {id: 0, name: "個"},
     {id: 1, name: "g"},
     {id: 2, name: "ml"},
-    {id: 0, name: "本"},
-    {id: 0, name: "枚"},
-    {id: 0, name: "匹"},
-    {id: 0, name: "杯"},
-    {id: 0, name: "丁"}
+    {id: 3, name: "本"},
+    {id: 4, name: "枚"},
+    {id: 5, name: "匹"},
+    {id: 6, name: "杯"},
+    {id: 7, name: "丁"}
     ]
 
   include ActiveHash::Associations
