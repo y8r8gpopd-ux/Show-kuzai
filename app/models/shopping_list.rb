95e70ecc4e22e8dd2aead9ba1e4472f51b9a1d1b
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-05-21 17:58:28 +0900

    お買い物リストボタン実装

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -3,4 +3,6 @@ class ShoppingList < ApplicationRecord
   belongs_to :recipe
   has_many :ingredients, through: :shopping_list_items
   has_many :shopping_list_items, dependent: :destroy
+
+  validates :recipe_id, uniqueness: { scope: :user_id }
 end
