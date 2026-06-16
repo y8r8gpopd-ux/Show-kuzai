4529cf0be7d5790362af45a0910f368b7adde619
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-06-16 21:48:46 +0900

    factorybotの設定と簡単なテストの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email {"test@test"}
+    password {"test123"}
+    password_confirmation {password}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @user = FactoryBot.build(:user)
+  end
+
+  describe "ユーザー登録" do
+
+    context "新規登録できる時" do
+
+      it "入力内容が正しければ新規登録できる" do
+         expect(@user).to be_valid
+      end
+
+    end
+  end
 end
