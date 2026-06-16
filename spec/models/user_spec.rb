ccde8636c4f4faa32ccc661d42d12c427ddfe309
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-06-16 22:33:18 +0900

    ユーザー登録に関するテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe User, type: :model do
          expect(@user).to be_valid
       end
 
+      it "登録時の管理者権限(admin)はfalseである" do
+        user = User.create!(email: "test@example.com",password: "password")
+        expect(user.reload.admin).to be false
+      end
+
     end
 
     context "新規登録できない時" do
@@ -23,12 +28,48 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
 
-      it "メールアドレスに@を含まない" do
+      it "メールアドレスに@を含まない場合登録できない" do
         @user.email = "test123"
         @user.valid?
         expect(@user.errors.full_messages).to include("Email is invalid")
       end
 
+      it "すでに登録されているメールアドレスでは登録できない" do
+        another_user = FactoryBot.create(:user)
+        @user.email = another_user.email
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it "パスワードが入力されていない" do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it "パスワードが6文字以上でなければ登録できない" do
+        @user.password = "a1234"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+      it "パスワードが128文字以下でなければ登録できない" do
+        @user.password = "a" * 129
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+      end
+
+      it "パスワードは英数字以外使用できない" do
+        @user.password = "テストテスト"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
+      it "パスワードの再入力が不一致では登録できない" do
+        @user.password_confirmation = "test111"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
 
     end
   end
